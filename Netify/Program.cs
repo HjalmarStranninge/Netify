db291e2a14b915f073cfd1486a19fc2d308c5f8c
sjood <sjood@NIKLASDATA>
2024-01-10 15:51:20 +0100

    -Fixing endpoints in program

--- Netify/Program.cs
+++ Netify/Program.cs
@@ -12,9 +12,20 @@ namespace Netify
             builder.Services.AddDbContext<NetifyContext>(opt => opt.UseSqlServer(connectionString));
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Welcome to the Netify Api. This projects involves creating a minimal API around the Spotify open access api.\n" +
+"Retrieve All Users\r\nRetrieve Genres Linked to a Specific Person\r\nRetrieve Artists Linked to a Specific Person\r\nRetrieve Songs Linked to a Specific Person\r\nConnect a Person to a New Genre, Artist, and Song");
 
-            app.Run();
+            // GET
+            app.MapGet("/users", () => ""); // Get all users in db
+            app.MapGet("/user/{userId}", () => ""); // Get a specific user
+            app.MapGet("/user/{userId}/genres", () => ""); // Get a specfic user and their liked genres
+            app.MapGet("/users/{userId}/artists", () => ""); // Get a specific user and ther liked artists
+            app.MapGet("/users/{userId}/songs", () => ""); // Get a specific user and ther liked songs
+
+            // POST
+            app.MapPost("/user/{userId}/genre/{genreId}", () => ""); // Add a specific user to a genre
+            app.MapPost("/user/{userId}/artist/{artistId}", () => ""); // Add a specific user to a artist
+            app.MapPost("/user/{userId}/songs/{songId}", () => ""); // Add a specific user to a song
         }
     }
 }
