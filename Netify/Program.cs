58a3833bc3f2a21a5158b6d4aec1aaab5fc08ef2
Huan <98620169+bentonaw@users.noreply.github.com>
2024-01-29 17:18:06 +0100

    Quick realigning of endpoints

--- Netify/Program.cs
+++ Netify/Program.cs
@@ -28,16 +28,16 @@ namespace Netify
 
             // GET
             app.MapGet("/users", UserHandler.ListUsers); // Get all users in db
-            app.MapGet("/users/{userId}", UserHandler.ViewUser); // Get a specific user
-            app.MapGet("/users/search", UserHandler.SearchUsers); // Search users,"?query={name}"
-            app.MapGet("/users/{userId}/genres", UserHandler.UserGenres); // Get a specfic user and their liked genres
-            app.MapGet("/users/{userId}/artists", UserHandler.UserArtists); // Get a specific user and their liked artists
-            app.MapGet("/users/{userId}/tracks", UserHandler.UserTracks); // Get a specific user and their liked tracks
+            app.MapGet("/user/{userId}", UserHandler.ViewUser); // Get a specific user
+            app.MapGet("/user/search", UserHandler.SearchUsers); // Search users,"?query={name}"
+            app.MapGet("/user/{userId}/genres", UserHandler.UserGenres); // Get a specfic user and their liked genres
+            app.MapGet("/user/{userId}/artists", UserHandler.UserArtists); // Get a specific user and their liked artists
+            app.MapGet("/user/{userId}/tracks", UserHandler.UserTracks); // Get a specific user and their liked tracks
 
             // POST
-            app.MapPost("/users/{userId}/genres/{genreId}", () => ""); // Add a specific user to a genre
-            app.MapPost("/users/{userId}/artists/{artistId}", () => ""); // Add a specific user to a artist
-            app.MapPost("/users/{userId}/tracks/{trackId}", () => ""); // Add a specific user to a tracks
+            app.MapPost("/user/{userId}/genres/{genreId}", () => ""); // Add a specific user to a genre
+            app.MapPost("/user/{userId}/artists/{artistId}", () => ""); // Add a specific user to a artist
+            app.MapPost("/user/{userId}/tracks/{trackId}", () => ""); // Add a specific user to a tracks
 
             app.MapGet("/tracksearch", async (ISpotifyService handler, string query, int? offset) => {
                 if (String.IsNullOrEmpty(query))
